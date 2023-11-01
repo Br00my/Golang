a4e0ebc7b86b827a93cbf64e9a0cb38b2342eecc
Amir13N <meepopopo22@mail.ru>
2023-11-01 22:22:16 +0600

    chapter 17

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -9,12 +9,11 @@ func (app *application) routes() http.Handler {
 	router.NotFound = http.HandlerFunc(app.notFoundResponse)
 
 	router.MethodNotAllowed = http.HandlerFunc(app.methodNotAllowedResponse)
-	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthcheckHandler)
-	router.HandlerFunc(http.MethodPost, "/v1/products", app.createProductHandler)
-	router.HandlerFunc(http.MethodGet, "/v1/products", app.listProductsHandler)
-	router.HandlerFunc(http.MethodGet, "/v1/products/:id", app.showProductHandler)
-	router.HandlerFunc(http.MethodPatch, "/v1/products/:id", app.updateProductHandler)
-	router.HandlerFunc(http.MethodDelete, "/v1/products/:id", app.deleteProductHandler)
+	router.HandlerFunc(http.MethodGet, "/v1/products", app.requirePermission("products:read", app.listProductsHandler))
+	router.HandlerFunc(http.MethodPost, "/v1/products", app.requirePermission("products:write", app.createProductHandler))
+	router.HandlerFunc(http.MethodGet, "/v1/products/:id", app.requirePermission("products:read", app.showProductHandler))
+	router.HandlerFunc(http.MethodPatch, "/v1/products/:id", app.requirePermission("products:write", app.updateProductHandler))
+	router.HandlerFunc(http.MethodDelete, "/v1/products/:id", app.requirePermission("products:write", app.deleteProductHandler))
 
 	router.HandlerFunc(http.MethodPost, "/v1/users", app.registerUserHandler)
 	router.HandlerFunc(http.MethodPut, "/v1/users/activated", app.activateUserHandler)
